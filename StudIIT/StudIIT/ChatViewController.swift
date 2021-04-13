2c4894dbe7b1d93f0aa3e009d4af615a09c2fde5
Quan Le <qle2@hawk.iit.edu>
2021-04-12 23:35:21 -0400

    no message

--- StudIIT/StudIIT/ChatViewController.swift
+++ StudIIT/StudIIT/ChatViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MainPageViewController.swift
+//  ChatViewController.swift
 //  StudIIT
 //
 //  Created by Ashley Le on 4/12/21.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainPageViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
